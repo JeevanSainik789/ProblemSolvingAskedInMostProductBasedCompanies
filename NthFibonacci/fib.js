09e575ff681191fcc08be2d683afe988eaea240f
Naveen <70085321+naveen701526@users.noreply.github.com>
2023-10-05 22:56:16 +0530

    Create fib.js

---
+++ NthFibonacci/fib.js
@@ -0,0 +1,18 @@
+function fibonacci(n) {
+  if (n === 0) {
+    return 0;
+  }
+  
+  const fib = [0, 1];
+  
+  for (let i = 2; i <= n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  
+  return fib[n];
+}
+
+console.log(fibonacci(0)); // Output: 0
+console.log(fibonacci(1)); // Output: 1
+console.log(fibonacci(5)); // Output: 5
+console.log(fibonacci(10)); // Output: 55
